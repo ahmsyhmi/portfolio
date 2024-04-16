d7a33847c864eebd5daca20094f149579f508e52
ahmsyhmi <ahmsyhmi@gmail.com>
2024-04-17 07:07:47 +0800

    update full about

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -3,26 +3,33 @@ import AutoType from "./AutoType";
 
 export default function Nav() {
     return (
-        <div className="flex justify-between bg-black h-full w-full space-x-2 px-10 pt-5 pb-5">
-
-            <ul className="space-x-2 item-center text-green-400">
-                <li className="item-center text-xl font-bold">
+        <div className="flex flex-col md:flex-row justify-between bg-black h-full w-full space-y-2 md:space-y-0 px-10 pt-5 pb-5 md:pb-5 md:items-center">
+            <ul className="flex justify-center md:justify-start space-x-2 md:space-x-6 md:mr-auto text-green-400 text-xl font-bold">
+                <li>
                     <AutoType />
                 </li>
             </ul>
 
-            <ul className="item-center flex gap-6 text-green-400 text-xl">    
-                <li className="">
-                    <a href="#" className="">About.txt</a>
+            <ul className="flex justify-center md:justify-end space-x-6 text-green-400 text-xl">
+                <li>
+                    <a href="#" className="">
+                        About.txt
+                    </a>
                 </li>
-                <li className="">
-                    <a href="#" className="">Skills.txt</a>
+                <li>
+                    <a href="#" className="">
+                        Skills.txt
+                    </a>
                 </li>
-                <li className="">
-                    <a href="#" className="">Education.txt</a>
+                <li>
+                    <a href="#" className="">
+                        Education.txt
+                    </a>
                 </li>
-                <li className="">
-                    <a href="#" className="">Certificates.txt</a>
+                <li>
+                    <a href="#" className="">
+                        Certificates.txt
+                    </a>
                 </li>
             </ul>
         </div>
